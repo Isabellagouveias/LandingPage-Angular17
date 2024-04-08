d935d3f6dc756610fe1cd0f34a7fdb4281e8763d
isabellagouveias <Isabellagouveias@gmail.com>
2024-04-08 10:12:23 -0300

    feat: cards e footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { BenefitComponent } from '../benefit/benefit.component';
 import { ButtonPrimaryComponent } from '../button-primary/button-primary.component';
+import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, ButtonPrimaryComponent],
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+    ButtonPrimaryComponent,
+    NewsletterFormComponent,
+    BenefitComponent,
+    FooterComponent,
+  ],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
